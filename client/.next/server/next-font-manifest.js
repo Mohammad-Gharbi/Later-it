9bc1eee61d0139f74fe4a94ae437d411e5dfcf90
Mohammed Gharbi <mohammedgharbi@icloud.com>
2023-06-04 15:16:31 +0100

    Delete client/.next directory

--- client/.next/server/next-font-manifest.js
+++
@@ -1,8 +0,0 @@
-self.__NEXT_FONT_MANIFEST={
-  "pages": {},
-  "app": {
-    "/Users/mohamedgharbi/Documents/Projects/Later-it/client/node_modules/next/font/google/target.css?{\"path\":\"app/layout.jsx\",\"import\":\"Inter\",\"arguments\":[{\"weight\":[\"400\",\"500\",\"700\"],\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": []
-  },
-  "appUsingSizeAdjust": true,
-  "pagesUsingSizeAdjust": false
-}
